8a0c16bc5c39af9af39e636b643574fe204cc7c5
Dan <daniel.murphy.1@gmail.com>
2020-07-23 23:16:43 -0400

    added decimal place funcitonality, began styling

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 const display = document.getElementById("display");
-display.readOnly = true;
+//display.readOnly = true;
 let replace = false;
 let num1 = null;
 let num2 = null;
 let operator = null;
+let memory = null;
 
-const btn = document.getElementById("button");
-btn.addEventListener("click", function () {
-  numberValue = document.getElementById("display").value;
-  console.log(numberValue);
+const decimal = document.getElementById("decimal");
+decimal.addEventListener("click", function (e) {
+  if (replace) {
+    display.textContent = null;
+    replace = false;
+  }
+  if (display.textContent.includes(".")) {
+    // display.textContent = display.textContent;
+    return display.textContent;
+  } else {
+    display.textContent = display.textContent + e.target.textContent;
+  }
+});
+
+const memAdd = document.getElementById("memory-add");
+memAdd.addEventListener("click", function () {
+  memory = display.textContent;
+});
+
+const memClear = document.getElementById("memory-clear");
+memClear.addEventListener("click", function () {
+  memory = null;
+});
+
+const memCall = document.getElementById("memory-call");
+memCall.addEventListener("click", function () {
+  if (memory) {
+    display.textContent = memory;
+  }
+  return display.textContent;
 });
 
 const clearBtn = document.getElementById("clear");
 clearBtn.addEventListener("click", function () {
-  display.value = parseFloat(0);
+  display.textContent = parseFloat(0);
   replace = false;
   operator = null;
   num1 = null;
@@ -24,78 +51,73 @@ const operatorBtns = document.querySelectorAll(".operator-button");
 operatorBtns.forEach(function (button) {
   button.addEventListener("click", function (e) {
     if (operator) {
-      
-        calculate();
-        num1 = parseFloat(display.value);
-        return operator = button.textContent;
-     
+      calculate();
+      num1 = parseFloat(display.textContent);
+      return (operator = button.textContent);
     }
-    num1 = parseFloat(display.value);
+    num1 = parseFloat(display.textContent);
     replace = true;
-    return operator = button.textContent;
-  }); 
+    return (operator = button.textContent);
+  });
 });
 
 const numberBtns = document.querySelectorAll(".number-button");
- numberBtns.forEach(function (numButton){
-    numButton.addEventListener("click", function(e){
-        if(replace){
-            display.value = null;
-            replace = false;
-        }
-        if(display.value === "0"){
-            display.value = parseFloat(e.target.textContent);
-        } else {
-            let tempNumber = display.value;
-            display.value = null;
-            display.value = parseFloat(tempNumber + e.target.textContent);
-        }
-        
-    })
-   
-})
+numberBtns.forEach(function (numButton) {
+  numButton.addEventListener("click", function (e) {
+    if (replace) {
+      display.textContent = null;
+      replace = false;
+    }
+    if (display.textContent === "0") {
+      display.textContent = parseFloat(e.target.textContent);
+    } else {
+      let tempNumber = display.textContent;
+      display.textContent = null;
+      display.textContent = parseFloat(tempNumber + e.target.textContent);
+    }
+  });
+});
 
 const equalBtn = document.getElementById("equal-button");
 equalBtn.addEventListener("click", calculate);
 
 function calculate() {
-  num2 = parseFloat(display.value);
+  num2 = parseFloat(display.textContent);
   let result = "";
   switch (operator) {
     case "+":
-    result = num1 + num2;
-      display.value = result;
+      result = num1 + num2;
+      display.textContent = result;
       replace = true;
       operator = null;
       num2 = null;
       return result;
     case "-":
-       result = num1 - num2;
-       display.value = result;
-       replace = true;
-       operator = null;
-       num2 = null;
-       return result;
-     
+      result = num1 - num2;
+      display.textContent = result;
+      replace = true;
+      operator = null;
+      num2 = null;
+      return result;
+
     case "/":
-       result = num1 / num2;
-       if( num2 === 0){
+      result = num1 / num2;
+      if (num2 === 0) {
         alert("Cannot divide by 0!");
-           return;
-       }
-       display.value = result;
-       replace = true;
-       operator = null;
-       num2 = null;
-       return result;
-      
+        return;
+      }
+      display.textContent = result;
+      replace = true;
+      operator = null;
+      num2 = null;
+      return result;
+
     case "x":
-        result = num1 * num2;
-        display.value = result;
-        replace = true;
-        operator = null;
-        num2 = null;
-        return result;
-      
+      result = num1 * num2;
+      display.textContent = result;
+      replace = true;
+      operator = null;
+      num2 = null;
+      return result;
   }
-};
+}
