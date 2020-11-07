2cb5c95b862cf46bef9cb9ec14b6eedeb1ab5f79
Dan <daniel.murphy.1@gmail.com>
2020-11-06 19:04:53 -0500

    grouped html element refereces and event listeners together, respectively

--- script.js
+++ script.js
@@ -1,151 +1,64 @@
 const display = document.getElementById("display");
+const equalBtn = document.getElementById("equal-button");
+const decimal = document.getElementById("decimal");
+const memAdd = document.getElementById("memory-add");
+const memClear = document.getElementById("memory-clear");
+const memCall = document.getElementById("memory-call");
+const clearBtn = document.getElementById("clear");
+const operatorBtns = document.querySelectorAll(".operator-button");
+const numberBtns = document.querySelectorAll(".number-button");
 let replace = false;
 let num1 = null;
 let num2 = null;
 let operator = null;
 let memory = null;
 
-//decimal point functionality
-const decimal = document.getElementById("decimal");
-decimal.addEventListener("click", function (e) {
-    addDecimalPoint(e.target.textContent);
-});
-document.addEventListener("keydown", function(e){
-    if (e.keyCode === 110){
-        addDecimalPoint(e.key);
-    }
-});
 function addDecimalPoint(decimalTarget){
-    if (replace) {
-        display.textContent = null;
-        replace = false;
-      }
-      if (display.textContent.includes(".")) {
-        return display.textContent;
-      } else {
-        display.textContent = display.textContent + decimalTarget;
-      }
+  if (replace) {
+    display.textContent = null;
+    replace = false;
+  }
+  
+  if (display.textContent.includes(".")) {
+    return display.textContent;
+  } else {
+    display.textContent = display.textContent + decimalTarget;
+  }
 }
 
-//memory buttons functionality
-const memAdd = document.getElementById("memory-add");
-memAdd.addEventListener("click", function () {
-  memory = display.textContent;
-});
-const memClear = document.getElementById("memory-clear");
-memClear.addEventListener("click", function () {
-  memory = null;
-});
-const memCall = document.getElementById("memory-call");
-memCall.addEventListener("click", function () {
-  if (memory) {
-    display.textContent = memory;
-  }
-  return display.textContent;
-});
-
-//cear button functionality
-const clearBtn = document.getElementById("clear");
-clearBtn.addEventListener("click", function () {
-  display.textContent = parseFloat(0);
-  replace = false;
-  operator = null;
-  num1 = null;
-  num2 = null;
-});
-
-//operators functionality
-const operatorBtns = document.querySelectorAll(".operator-button");
-operatorBtns.forEach(function (button) {
-  button.addEventListener("click", function (e) {
-    if (operator) {
-      calculate();
-      num1 = parseFloat(display.textContent);
-      return (operator = button.textContent);
-    }
-    num1 = parseFloat(display.textContent);
-    replace = true;
-    return (operator = button.textContent);
-  });
-});
-document.addEventListener("keydown", function (e) {
-  if (
-    e.keyCode === 106 ||
-    e.keyCode === 107 ||
-    e.keyCode === 109 ||
-    e.keyCode === 111
-  ) {
-    if (operator) {
-      calculate();
-      num1 = parseFloat(display.textContent);
-      keypadOperatorSelect(e);
-    }else {
-   
-    num1 = parseFloat(display.textContent);
-    replace = true;
-    keypadOperatorSelect(e);
-  }
-} else{
-    return;
-}
-})
 function keypadOperatorSelect(e){
-    switch (e.keyCode) {
-        case 107:
-          operator = "+";
-          return operator;
-        case 109:
-          operator = "-";
-          return operator;
+  switch (e.keyCode) {
+    case 107:
+      operator = "+";
+      return operator;
+    case 109:
+      operator = "-";
+      return operator;
 
-        case 111:
-          operator = "/";
-          return operator;
+    case 111:
+      operator = "/";
+      return operator;
 
-        case 106:
-          operator = "*";
-          return operator;
-          default:
-      }
+    case 106:
+      operator = "*";
+      return operator;
+      default:
+    }
 }
 
-//numbers functionality
-const numberBtns = document.querySelectorAll(".number-button");
-numberBtns.forEach(function (numButton) {
-  numButton.addEventListener("click", function (e) {
-    addNumberButton(e.target.textContent);
-  });
-});
-document.addEventListener("keydown", function (e) {
-    if(e.keyCode >= 96 && e.keyCode<= 105){
-        addNumberButton(e.key);
-        }else{
-            return;
-        }
-})
 function addNumberButton (numberTarget){
-    if (replace) {
-        display.textContent = null;
-        replace = false;
-      }
-      if (display.textContent === "0") {
-        display.textContent = numberTarget;
-      } else {
-        display.textContent = (display.textContent + numberTarget);
-      }
+  if (replace) {
+      display.textContent = null;
+      replace = false;
+  }
+  if (display.textContent === "0") {
+    display.textContent = numberTarget;
+  } else {
+    display.textContent = (display.textContent + numberTarget);
+  }
 }
 
-//equals functionality
-const equalBtn = document.getElementById("equal-button");
-equalBtn.addEventListener("click", calculate);
-document.addEventListener("keydown", function(e){
-  if(e.keyCode === 13){
-      e.preventDefault();
-      calculate();
-  } else{
-      return;
-  }
-})
+
 function calculate() {
   num2 = parseFloat(display.textContent);
   let result = "";
@@ -185,3 +98,97 @@ function calculate() {
       return result;
   }
 }
+
+//equals functionality
+equalBtn.addEventListener("click", calculate);
+document.addEventListener("keydown", function(e){
+  if(e.keyCode === 13){
+    e.preventDefault();
+    calculate();
+  } else {
+    return;
+  }
+});
+
+//numbers functionality
+numberBtns.forEach(function (numButton) {
+  numButton.addEventListener("click", function (e) {
+    addNumberButton(e.target.textContent);
+  });
+});
+document.addEventListener("keydown", function (e) {
+  if(e.keyCode >= 96 && e.keyCode<= 105){
+    addNumberButton(e.key);
+    }else{
+        return;
+  }
+});
+
+//operators functionality
+operatorBtns.forEach(function (button) {
+  button.addEventListener("click", function (e) {
+    if (operator) {
+      calculate();
+      num1 = parseFloat(display.textContent);
+      return (operator = button.textContent);
+    }
+    num1 = parseFloat(display.textContent);
+    replace = true;
+    return (operator = button.textContent);
+  });
+});
+document.addEventListener("keydown", function (e) {
+  if (
+    e.keyCode === 106 ||
+    e.keyCode === 107 ||
+    e.keyCode === 109 ||
+    e.keyCode === 111
+  ) {
+    if (operator) {
+      calculate();
+      num1 = parseFloat(display.textContent);
+      keypadOperatorSelect(e);
+    }else {
+      num1 = parseFloat(display.textContent);
+      replace = true;
+      keypadOperatorSelect(e);
+    }
+  } else {
+    return;
+  }
+});
+
+//memory buttons functionality
+memAdd.addEventListener("click", function () {
+  memory = display.textContent;
+});
+
+memClear.addEventListener("click", function () {
+  memory = null;
+});
+
+memCall.addEventListener("click", function () {
+  if (memory) {
+    display.textContent = memory;
+  }
+  return display.textContent;
+});
+
+//cear button functionality
+clearBtn.addEventListener("click", function () {
+  display.textContent = parseFloat(0);
+  replace = false;
+  operator = null;
+  num1 = null;
+  num2 = null;
+});
+
+//decimal point functionality
+decimal.addEventListener("click", function (e) {
+  addDecimalPoint(e.target.textContent);
+});
+document.addEventListener("keydown", function(e){
+  if (e.keyCode === 110){
+      addDecimalPoint(e.key);
+  }
+});
